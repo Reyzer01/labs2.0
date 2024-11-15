0c8f741a0b2fdddcb4e09ebd4bcbd9a47f4ce7a3
Reyzer01 <reyzerlure@gmail.com>
2024-11-15 21:00:55 +0200

    Update main.cpp

--- work8/main.cpp
+++ work8/main.cpp
@@ -5,13 +5,13 @@
 #include <set>
 #include <algorithm>
 #include <map>
-// Ôóíêöèÿ äëÿ ïðîâåðêè, ÿâëÿåòñÿ ëè ñèìâîë ñîãëàñíîé
+//  Функция для проверки, является ли символ согласной
 bool isConsonant(char c) {
     c = std::tolower(c);
     return isalpha(c) && !std::strchr("aeiou", c);
 }
 
-// Ôóíêöèÿ äëÿ ïîäñ÷åòà ðàçëè÷íûõ ñîãëàñíûõ â ñëîâå
+//  Функция для подсчета различных согласных в слове
 int countDistinctConsonants(const std::string& word) {
     std::set<char> consonants;
     for (char c : word) {
@@ -29,7 +29,7 @@ int main(){
     std::set<std::string> uniqueWords;
     std::string line;
 
-    // ×òåíèå ôàéëà è îáðàáîòêà ñëîâ
+    // Чтение файла и обработка слов
     while (std::getline(inputFile, line)){
         std::istringstream iss(line);
         std::string word;
@@ -41,18 +41,18 @@ int main(){
     }
     std::string word;
     std::map<std::string, int> wordConsonantCount;
-    // Ïåðåíîñ óíèêàëüíûõ ñëîâ â âåêòî
+    // Перенос уникальных слов в вектор
     for (const auto& word : uniqueWords){
         words.push_back(word);
     }
 
-    // Ñîðòèðóåì ñëîâà ïî êîëè÷åñòâó ðàçëè÷íûõ ñîãëàñíûõ
+    // Сортировка слова по количеству различных согласных
     std::vector<std::pair<std::string, int>> sortedWords(wordConsonantCount.begin(), wordConsonantCount.end());
     std::sort(sortedWords.begin(), sortedWords.end(), [](const auto& a, const auto& b) {
         return a.second > b.second; // Ñîðòèðîâêà ïî óáûâàíèþ
         });
 
-    // Îãðàíè÷èâàåì êîëè÷åñòâî ñëîâ äî N (2000)
+    // Ограние количества слов до N (2000)
     int N = std::min(2000, static_cast<int>(sortedWords.size()));
 
     for (int i = 0; i < N; ++i) {
