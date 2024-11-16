baf9dc382f949eac054aa6ce455075a06a77cea7
Reyzer01 <reyzerlure@gmail.com>
2024-11-16 09:21:35 +0200

    Update func.hpp

--- work7/func.hpp
+++ work7/func.hpp
@@ -1,5 +1,4 @@
-#ifndef FUNC_HPP
-#define FUNC_HPP
+#pragma once
 
 const int MAX_SIZE = 100; // Максимальный размер матрицы
 
@@ -8,5 +7,3 @@ void processMatrix(int matrix[MAX_SIZE][MAX_SIZE], int n);
 void writeMatrix(int matrix[MAX_SIZE][MAX_SIZE], int n);
 bool isPrime(int number);
 int countPrimes(const int row[MAX_SIZE], int size);
-
-#endif // FUNC_HPP
